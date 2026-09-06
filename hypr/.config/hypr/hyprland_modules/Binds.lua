936729f6f0353ccdbb371c12eaf9c237fe1ddb87
lSnozin1 <thiaguim3291@gmail.com>
2026-09-06 22:40:17 +0000

    Sep 6, 2026, 7:40 PM

--- hypr/.config/hypr/hyprland_modules/Binds.lua
+++ hypr/.config/hypr/hyprland_modules/Binds.lua
@@ -61,8 +61,8 @@ end
 hl.bind(mainMod .. " + S", 						hl.dsp.workspace.toggle_special("magic"), { description = "Toggle special workspace 'magic'" })
 hl.bind(mainMod .. " + SHIFT + S", 				hl.dsp.window.move({ workspace = "special:magic" }), { description = "Move window to special workspace 'magic'" })
 
-hl.bind(mainMod .. " + G + 1", 						hl.dsp.workspace.toggle_special("kitty"), { description = "Toggle special workspace 'kitty'" })
-hl.bind(mainMod .. " + SHIFT + D", 				hl.dsp.window.move({ workspace = "special:kitty" }), { description = "Move window to special workspace 'kitty'" })
+hl.bind(mainMod .. " + HOME", 						hl.dsp.workspace.toggle_special("kitty"), { description = "Toggle special workspace 'kitty'" })
+hl.bind(mainMod .. " + SHIFT + HOME", 				hl.dsp.window.move({ workspace = "special:kitty" }), { description = "Move window to special workspace 'kitty'" })
 
 -------------------
 ---- UTILITIES ----
