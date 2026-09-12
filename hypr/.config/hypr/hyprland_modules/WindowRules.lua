3b0458e878b6a7c23e21fbc6a31606d72195fbe0
lSnozin1 <thiaguim3291@gmail.com>
2026-09-12 04:53:47 +0000

    Sep 12, 2026, 1:53 AM

--- hypr/.config/hypr/hyprland_modules/WindowRules.lua
+++ hypr/.config/hypr/hyprland_modules/WindowRules.lua
@@ -116,6 +116,16 @@ local moveSoberRule = hl.window_rule({
 	workspace = WS.GAMES,
 })
 
+local moveDeltaruneRule = hl.window_rule({
+	name = "fix-deltarune-position",
+	match = { class = "steam_app_1690940" },
+	workspace = WS.GAMES,
+	float = true,
+	size = "1280, 960",
+	center = true,
+	move = {100, 100},
+})
+
 hl.exec_cmd("hyprctl keyword windowrulev2 'float,class:^(steam_app_1690940)$'")
 hl.exec_cmd("hyprctl keyword windowrulev2 'size 1280 960,class:^(steam_app_1690940)$'")
 hl.exec_cmd("hyprctl keyword windowrulev2 'center,class:^(steam_app_1690940)$'")
