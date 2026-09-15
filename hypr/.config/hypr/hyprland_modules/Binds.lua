de307e018587df272af2a54af33cd698ef6f65d3
lSnozin1 <thiaguim3291@gmail.com>
2026-09-15 21:37:58 +0000

    Sep 15, 2026, 6:37 PM

--- hypr/.config/hypr/hyprland_modules/Binds.lua
+++ hypr/.config/hypr/hyprland_modules/Binds.lua
@@ -18,8 +18,8 @@ hl.bind(mainMod .. " + SHIFT + E", 		hl.dsp.exec_cmd(SECONDARY_FILE_MANAGER), { 
 
 hl.bind(mainMod .. " + R", 				hl.dsp.exec_cmd(MENU), { description = "Launch menu" })
 
--- hl.bind(mainMod .. " + B",				hl.dsp.exec_cmd(MAIN_RESOURCE_MANAGER), { description = "Launch Main Resource Manager"})
--- hl.bind(mainMod .. " + SHIFT + B",		hl.dsp.exec_cmd(TERMINAL .. " --single-instance " .. SECONDARY_RESOURCE_MANAGER), { description = "Launch Secondary Resource Manager"})
+hl.bind(mainMod .. " + B",				hl.dsp.exec_cmd(MAIN_RESOURCE_MANAGER), { description = "Launch Main Resource Manager"})
+hl.bind(mainMod .. " + SHIFT + B",		hl.dsp.exec_cmd(TERMINAL .. " --single-instance " .. SECONDARY_RESOURCE_MANAGER), { description = "Launch Secondary Resource Manager"})
 -----------------
 ---- WINDOWS ----
 -----------------
