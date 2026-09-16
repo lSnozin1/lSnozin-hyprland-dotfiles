863353a7371072f3adafe12a2ebde0ed5e93cf4e
lSnozin1 <thiaguim3291@gmail.com>
2026-09-16 02:53:38 +0000

    Sep 15, 2026, 11:53 PM

--- hypr/.config/hypr/hyprland_modules/AutoStarts.lua
+++ hypr/.config/hypr/hyprland_modules/AutoStarts.lua
@@ -21,8 +21,12 @@ hl.on("hyprland.start", function()
 	-- headless kitty (not that that important but helps with kitty performance and resource usage by a noticiable margin)
 	hl.exec_cmd("kitty --start-as=hidden --single-instance")
 
-	-- permanent kitty for special workspace 'kitty'
+	-- permanent kitty for special workspace 'kitty' (not rlly important)
 	hl.exec_cmd("kitty --class kitty-permanent --single-instance")
+
+	-- permanent btop for special workspace 'btop' (not rlly important)
+	hl.exec_cmd("alacritty --class btop-permanent -e btop")
+
 	-- wallpaper (not that important)
 	hl.exec_cmd("sleep 5 && ~/Applications/waywallen-0.3.7-x86_64.AppImage --no-ui")
 	
