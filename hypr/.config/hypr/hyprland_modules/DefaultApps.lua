815eeac9c0e2bd25b23e4cd8491bad942aff3553
lSnozin1 <thiaguim3291@gmail.com>
2026-09-16 01:16:39 -0300

    'minor' changes

--- hypr/.config/hypr/hyprland_modules/DefaultApps.lua
+++ hypr/.config/hypr/hyprland_modules/DefaultApps.lua
@@ -12,8 +12,8 @@ BROWSER         = "firefox"
 --EDITOR        = "gnome-text-editor --new-window"
 --CALCULATOR    = "gnome-calculator"
 MENU            = "XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:" .. os.getenv("HOME") .. "/.local/share/flatpak/exports/share rofi -show combi -combi-modi 'drun,run' -show-icons"
-MAIN_RESOURCE_MANAGER = "missioncenter"
-SECONDARY_RESOURCE_MANAGER = "resources"
+MAIN_RESOURCE_MANAGER = "resources"
+SECONDARY_RESOURCE_MANAGER = "missioncenter"
 TERTIARY_RESOURCE_MANAGER = "btop"
 
 -- Monitors
