12a9c6ff847237802001c20b4577781b959c3485
lSnozin1 <thiaguim3291@gmail.com>
2026-09-21 20:49:47 +0000

    Sep 21, 2026, 5:49 PM

--- hypr/.config/hypr/hyprland_modules/Binds.lua
+++ hypr/.config/hypr/hyprland_modules/Binds.lua
@@ -20,7 +20,7 @@ hl.bind(mainMod .. " + SHIFT + E", 		hl.dsp.exec_cmd(SECONDARY_FILE_MANAGER), { 
 
 hl.bind(mainMod .. " + R", 				hl.dsp.exec_cmd(MENU), { description = "Launch menu" })
 
-hl.bind(mainMod .. " + B", hl.dsp.exec_cmd("pidof resources > /dev/null && hyprctl dispatch 'hl.dsp.workspace.toggle_special(\"resources\")' || " .. MAIN_RESOURCE_MANAGER), { description = "Focus or Open Main Resource Manager" })
+hl.bind(mainMod .. " + B", 				hl.dsp.exec_cmd("pidof resources > /dev/null && hyprctl dispatch 'hl.dsp.workspace.toggle_special(\"resources\")' || " .. MAIN_RESOURCE_MANAGER), { description = "Focus or Open Main Resource Manager" })
 
 
 --	hl.bind(mainMod .. " + SHIFT + B",		hl.dsp.exec_cmd(SECONDARY_RESOURCE_MANAGER), { description = "Launch Secondary Resource Manager"})
