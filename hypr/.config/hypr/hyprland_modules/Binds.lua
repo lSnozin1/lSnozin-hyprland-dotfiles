aa00aab61b563db57e7c38d0f3ac520867233a98
lSnozin1 <thiaguim3291@gmail.com>
2026-09-24 17:49:21 +0000

    Sep 24, 2026, 2:49 PM

--- hypr/.config/hypr/hyprland_modules/Binds.lua
+++ hypr/.config/hypr/hyprland_modules/Binds.lua
@@ -110,7 +110,8 @@ hl.bind(mainMod .. " + KP_Subtract", 		hl.dsp.exec_cmd("wpctl set-volume @DEFAUL
 hl.bind(mainMod .. " + M",				hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"), { description = "Exit Hyprland" })
 
 -- Locks the Pc with Noctalia Lockscreen
-hl.bind(mainMod .. " + N",				hl.dps.exec_cmd("noctalia msg session lock"), {description = "Lock PC"})
+hl.bind(mainMod .. " + N",				hl.dsp.exec_cmd("noctalia msg session lock"), { description = "Lock PC" })
+
 -----------------
 ---- SUBMAPS ----
 -----------------
